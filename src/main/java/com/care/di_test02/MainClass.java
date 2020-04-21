71992b56df3d49f24dcd517295d11b177edd29be
123 <123@123>
2020-04-21 21:35:33 +0900

    spring MainClass 주석 추가

--- src/main/java/com/care/di_test02/MainClass.java
+++ src/main/java/com/care/di_test02/MainClass.java
@@ -6,22 +6,31 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class MainClass {
 
-	@SuppressWarnings("resource")
 	public static void main(String[] args) {
+		//스캐너
+		Scanner scn = new Scanner(System.in);
+		
+		//xml연결
 		String config = "classpath:application_test02.xml";
-		GenericXmlApplicationContext cont = new GenericXmlApplicationContext(config);
-		
+		GenericXmlApplicationContext cont = new GenericXmlApplicationContext(config);		
 		SaveClass sc = cont.getBean("sc", SaveClass.class);
-		System.out.print("숫자1 입력: ");
-		sc.setNum1(new Scanner(System.in).nextInt());
-		System.out.print("숫자2 입력: ");
-		sc.setNum2(new Scanner(System.in).nextInt());
-		System.out.print("연산자 입력: ");
-		sc.setOp(new Scanner(System.in).next());
 		
+		//값 입력 받기
+		System.out.print("숫자1 입력: ");		
+		sc.setNum1(scn.nextInt());
+		
+		System.out.print("숫자2 입력: ");
+		sc.setNum2(scn.nextInt());
+		
+		System.out.print("연산자 입력: ");
+		sc.setOp(scn.next());
+		
+		//계산, 결과 출력
 		sc.operationClass();
 		sc.printClass();
 		
+		//스캐너 닫기
+		scn.close();
 	}
 
 }
