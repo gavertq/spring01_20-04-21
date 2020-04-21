2c7c6f87eb19030d001b54dcdb27349cb1ad36dd
123 <123@123>
2020-04-21 21:40:08 +0900

    spring SaveClass 주석 추가

--- src/main/java/com/care/di_test02/SaveClass.java
+++ src/main/java/com/care/di_test02/SaveClass.java
@@ -1,13 +1,13 @@
 package com.care.di_test02;
 
 public class SaveClass {
-	private int num1, num2, result;
-	private String op;
-	private PrintClass pc;
-	private OperationClass oc;
+	private int num1, num2, result;	//입력받을 숫자1, 입력받을 숫자2, 결과값 변수
+	private String op;				//입력받을 연산자 변수
+	private PrintClass pc;			//출력 객체
+	private OperationClass oc;		//연산 객체
 	
-	public void operationClass() { result = oc.operation(num1, num2, op);	}
-	public void printClass() {	pc.printFunc(num1, op, num2, result);}
+	public void operationClass() { result = oc.operation(num1, num2, op);}	//result = 연산결과
+	public void printClass() {	pc.printFunc(num1, op, num2, result);}		//출력
 	
 	public int getNum1() {return num1;}
 	public void setNum1(int num1) {	this.num1 = num1;}
@@ -16,12 +16,12 @@ public class SaveClass {
 	public void setNum2(int num2) {	this.num2 = num2;}
 	
 	public int getResult() {return result;}
-	public void setResult(int result) {	this.result = result;}
-	
+	public void setResult(int result) {	this.result = result;}	
 	
 	public String getOp() {	return op;}
 	public void setOp(String op) {	this.op = op;}
 	
+	//연산, 출력 객체 연결해줄 set,get
 	public PrintClass getPc() {	return pc;}
 	public void setPc(PrintClass pc) {	this.pc = pc;}
 	
