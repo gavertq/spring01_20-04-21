694c65b4e61701a59cb5f8dd8908f9184203194e
123 <123@123>
2020-04-22 21:29:29 +0900

    spring 문제 주석 추가

--- src/main/java/com/care/di_test03/MainClass.java
+++ src/main/java/com/care/di_test03/MainClass.java
@@ -9,8 +9,9 @@ public class MainClass {
 	public static void main(String[] args) {
 		String conf = "classpath:application_Student.xml";
 		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext(conf);
-		StudentClass sc = ctx.getBean("sc",StudentClass.class);
 		
+		//xml에서 생성한 객체 sc 연결
+		StudentClass sc = ctx.getBean("sc",StudentClass.class);		
 		//food를 xml파일에서 초기화 하게되면 필요없는 부분
 		ArrayList<String> food = new ArrayList<String>();
 		food.add("1");		food.add("2");		food.add("3");		
@@ -18,6 +19,7 @@ public class MainClass {
 		//		
 		sc.execute();
 		
+		//xml에서 생성한 객체 sc02 연결
 		StudentClass sc02 = ctx.getBean("sc02",StudentClass.class);
 		sc02.execute();
 	}
